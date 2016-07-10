51e12884a92a4a2119ae484d20fe28801d299376
ffbli666 <ffbli666@gmail.com>
2016-07-10 17:29:23 +0800

    完成測試案例 一二集各買了一本_第三集買了兩本_價格應為_370 完成測試案例第一集買了一本_第二三集各買了兩本_價格應為_460()

--- PotterShoppingChart/Sell.cs
+++ PotterShoppingChart/Sell.cs
@@ -9,24 +9,50 @@ namespace PotterShoppingChart
     {
         public double CalculatePrice(List<Book> books)
         {
+            //取得某一隻的最大數量
+            var countList = from b in books
+                            group b by b.Name into g
+                            select new
+                            {
+                                g.Key,
+                                Count = g.Count()
+                            };
+            int maxCount = countList.Max(x => x.Count);
+            //分類不同名字的集合,並計算該集合的 total price
+            List<double> total = new List<double> { };
             IEnumerable<IGrouping<String, Book>> query = books.GroupBy(x => x.Name);
-            double discount = 1.0;
-            switch (query.Count())
+            for (int i=0; i<maxCount; i++)
             {
-                case 2:
-                    discount = 0.95;
-                    break;
-                case 3:
-                    discount = 0.9;
-                    break;
-                case 4:
-                    discount = 0.8;
-                    break;
-                case 5:
-                    discount = 0.75;
-                    break;
+                int sum = 0; //不同名字的總和
+                int category = 0; //不同名字集合中有幾個不同名字
+                foreach (IGrouping<String, Book> bookGroup in query)
+                {
+                    var temp = bookGroup.ToList();
+                    if (i < temp.Count())
+                    {                        
+                        sum += temp[i].Price;
+                        category += 1;
+                    }
+                }
+                double discount = 1.0;
+                switch (category)
+                {
+                    case 2:
+                        discount = 0.95;
+                        break;
+                    case 3:
+                        discount = 0.9;
+                        break;
+                    case 4:
+                        discount = 0.8;
+                        break;
+                    case 5:
+                        discount = 0.75;
+                        break;
+                }
+                total.Add(sum * discount);
             }
-            return books.Sum(x => x.Price) * discount;
+            return total.Sum();
         }
     }
 }
