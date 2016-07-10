1fa68383cf1e49ec9cb6d0580f99389ec13c2c6f
ffbli666 <ffbli666@gmail.com>
2016-07-10 18:08:18 +0800

    重構

--- PotterShoppingChart/Sell.cs
+++ PotterShoppingChart/Sell.cs
@@ -9,7 +9,53 @@ namespace PotterShoppingChart
     {
         public double CalculatePrice(List<Book> books)
         {
-            //取得某一隻的最大數量
+            int maxCount = getMaxCountOfTheSameName(books);
+            //分類不同名字的集合,並計算該集合的 amount price
+            List<double> amount = new List<double> { };
+            IEnumerable<IGrouping<String, Book>> query = books.GroupBy(x => x.Name);
+            for (int i=0; i<maxCount; i++)
+            {
+                int sum = 0; //不同名字的總和
+                int different = 0; //集合中有幾個不同名字
+                foreach (IGrouping<String, Book> bookGroup in query)
+                {
+                    var temp = bookGroup.ToList();
+                    if (i < temp.Count())
+                    {                        
+                        sum += temp[i].Price;
+                        different += 1;
+                    }
+                }
+                amount.Add(sum * getDiscount(different));
+            }
+            return amount.Sum();
+        }
+        
+        private double getDiscount(int different)
+        {
+            //取得不同數量的折扣
+            double discount = 1.0;
+            switch (different)
+            {
+                case 2:
+                    discount = 0.95;
+                    break;
+                case 3:
+                    discount = 0.9;
+                    break;
+                case 4:
+                    discount = 0.8;
+                    break;
+                case 5:
+                    discount = 0.75;
+                    break;
+            }
+            return discount;
+        }
+
+        private int getMaxCountOfTheSameName(List<Book> books)
+        {
+            //取得同名書中的最大數量
             var countList = from b in books
                             group b by b.Name into g
                             select new
@@ -17,42 +63,7 @@ namespace PotterShoppingChart
                                 g.Key,
                                 Count = g.Count()
                             };
-            int maxCount = countList.Max(x => x.Count);
-            //分類不同名字的集合,並計算該集合的 total price
-            List<double> total = new List<double> { };
-            IEnumerable<IGrouping<String, Book>> query = books.GroupBy(x => x.Name);
-            for (int i=0; i<maxCount; i++)
-            {
-                int sum = 0; //不同名字的總和
-                int category = 0; //不同名字集合中有幾個不同名字
-                foreach (IGrouping<String, Book> bookGroup in query)
-                {
-                    var temp = bookGroup.ToList();
-                    if (i < temp.Count())
-                    {                        
-                        sum += temp[i].Price;
-                        category += 1;
-                    }
-                }
-                double discount = 1.0;
-                switch (category)
-                {
-                    case 2:
-                        discount = 0.95;
-                        break;
-                    case 3:
-                        discount = 0.9;
-                        break;
-                    case 4:
-                        discount = 0.8;
-                        break;
-                    case 5:
-                        discount = 0.75;
-                        break;
-                }
-                total.Add(sum * discount);
-            }
-            return total.Sum();
+            return countList.Max(x => x.Count);
         }
     }
 }
