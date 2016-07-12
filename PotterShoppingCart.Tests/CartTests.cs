06d80679607261a6a84aaeac11d07f4146186099
ffbli666 <ffbli666@gmail.com>
2016-07-12 22:56:33 +0800

    第三個測試案例一二三集各買了一本，價格應為100 * 3 * 0.9 = 270

--- PotterShoppingCart.Tests/CartTests.cs
+++ PotterShoppingCart.Tests/CartTests.cs
@@ -57,5 +57,30 @@ namespace PotterShoppingCart.Tests
             var expected = 190;
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod()]
+        public void CalculatePriceTest_一二三集各買了一本_價格應為_270()
+        {
+            //Scenario: 一二三集各買了一本，價格應為100 * 3 * 0.9 = 270
+            //Given 第一集買了 1 本
+            //And 第二集買了 1 本
+            //And 第三集買了 1 本
+            //And 第四集買了 0 本
+            //And 第五集買了 0 本
+            //When 結帳
+            //Then 價格應為 270 元
+            //arrange
+            var target = new Cart();
+            List<Product> products = new List<Product>
+            {
+                new Product { Name="哈利波特第一集", Price=100},
+                new Product { Name="哈利波特第二集", Price=100},
+                new Product { Name="哈利波特第三集", Price=100},
+            };
+            //act
+            var actual = target.CalculatePrice(products);
+            //assert
+            var expected = 270;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
